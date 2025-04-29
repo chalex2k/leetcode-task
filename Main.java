e04e9d53496fb1643a4d7f89cd930bf6be4e8abe
chalex2k <chalex2k@gmail.com>
2025-04-29 14:30:49 +0300

    Добавил 22222

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-        System.out.println("Hello world! 1111111 ");
+        System.out.println("Hello world! 22222222 ");
+        System.out.println("Hello world! 22222222 ");
     }
 }
